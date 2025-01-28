4cc41f251664fcb0d4f349d5105eac8b42ee4d40
OrdunaEmmanuel <emmanueloac@gmail.com>
2025-01-28 10:37:54 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require('dotenv');
+const app = express();
+
+
+dotenv.config();
+
+async function StartServer() {
+    const port = process.env.PORT || 3000; 
+    app.listen(port, () => {
+        console.log('=============================================');
+        console.log(`Servidor funcionando en http://localhost:${port}`);
+        console.log('=============================================');   
+    });
+}
+
+StartServer();
