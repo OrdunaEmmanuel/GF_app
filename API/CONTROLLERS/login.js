3421900952cc5b37dab5f5db3bdc55cd773470a5
JavierNC24 <145525009+JavierNC24@users.noreply.github.com>
2025-02-14 21:11:35 -0600

    Update login.js
    ya jala tu, ahora solo es con el numero de celular y contraseña, ahora bien cuando fakin cae la quincena
    >:c

--- API/CONTROLLERS/login.js
+++ API/CONTROLLERS/login.js
@@ -1,39 +1,61 @@
-const ClienteModel = require('../MODELS/CLIENTE.js')
-const PreventistaModel = require('../MODELS/PREVENTISTA.js')
-const SucursalModel = require('../MODELS/SUCURSAL.js')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const ClienteModel = require("../MODELS/CLIENTE.js");
+const SucursalModel = require("../MODELS/SUCURSAL.js");
+const PreventistaModel = require("../MODELS/PREVENTISTA.js");
 
-exports.login = async(req, res) =>{
-    try{
-        const {numero_celular , password} = req.body
-        const userTypes = [
-            { model: ClienteModel, type: 'CLIENTE', secret: process.env.JWT_SECRET_CLIENTE },
-            { model: PreventistaModel, type: 'PREVENTISTA', secret: process.env.JWT_SECRET_PREVENTISTA },
-            { model: SucursalModel, type: 'SUCURSAL', secret: process.env.JWT_SECRET_SUCURSAL }
-        ];
+require("dotenv").config();
 
-        let user = null
-        let userType = null
-        let secret_key = null
-        let userModel = null
-        for(const {model, type, secret} of userTypes){
-            user = await model.findOne({ numero_celular: numero_celular });
-            if(user){
-                userType = type
-                secret_key = secret
-                userModel = model
-                break;
-            }
+const SECRET_KEYS = {
+    C: process.env.JWT_SECRET_CLIENTE,
+    P: process.env.JWT_SECRET_PREVENTISTA,
+    S: process.env.JWT_SECRET_SUCURSAL
+};
+
+console.log("Secret Keys:", SECRET_KEYS);
+
+exports.login = async (req, res) => {
+    try {
+        const { numero_celular, password } = req.body;
+
+        let usuario = await ClienteModel.findOne({ numero_celular }) ||
+                      await SucursalModel.findOne({ numero_celular }) ||
+                      await PreventistaModel.findOne({ numero_celular });
+
+        if (!usuario) {
+            return res.status(404).json({ msg: "Usuario no encontrado" });
         }
-        if(!user){
-            return res.status(404).json({ msg: 'Usuario no encontrado' });
+
+        const passwordMatch = await bcrypt.compare(password, usuario.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ msg: "Contraseña incorrecta" });
         }
-        if(!await bcrypt.compare(password, user.password)){
-            return res.status(404).json({ msg: 'Contraseña incorrecta' });
+
+        const tipo_usuario = usuario.tipo_usuario;
+        const SECRET_KEY = SECRET_KEYS[tipo_usuario];
+
+        if (!SECRET_KEY) {
+            console.error("Error: No se encontró la clave secreta para el tipo de usuario:", tipo_usuario);
+            return res.status(500).json({ msg: "Error en la configuración del servidor" });
         }
-        
-    }catch(err){
-        res.status(500).json({ msg: err.message });
+
+        const token = jwt.sign(
+            { id: usuario._id, tipo_usuario },
+            SECRET_KEY,
+            { expiresIn: process.env.JWT_EXPIRATION_TIME || "8h" }
+        );
+
+        usuario.token = token;
+        await usuario.save();
+
+        res.status(200).json({
+            message: "Login exitoso, bienvenido usuario",
+            nombre: usuario.nombre,
+            token,
+            tipo_usuario
+        });
+
+    } catch (err) {
+        res.status(500).send(err.message);
     }
-}
+};
