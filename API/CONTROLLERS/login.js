cae241946caddfa25d1cfa6cfba4f66b0028659a
JavierNC24 <145525009+JavierNC24@users.noreply.github.com>
2025-02-14 19:10:40 -0600

    cambios
    reduccion del modelo a los datos necesarios e igual quite validaciones ya que no serien necesarias por parte de front

--- API/CONTROLLERS/login.js
+++ API/CONTROLLERS/login.js
@@ -1,48 +1,38 @@
-const Cliente = require('../MODELS/CLIENTE.js')
-const Preventista = require('../MODELS/PREEVENTISTA.JS')
-const Sucursal = require('../MODELS/SUCURSAL.js')
-const mongoose = require('mongoose')
-const jwt = require('jsonwebtoken')
+const ClienteModel = require('../MODELS/CLIENTE.js')
+const PreventistaModel = require('../MODELS/PREVENTISTA.js')
+const SucursalModel = require('../MODELS/SUCURSAL.js')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 exports.login = async(req, res) =>{
     try{
-        const { correoFL, passwordFL } = req.body
-        const TP_user = [
-            {model: Cliente, type: 'Cliente', secret: process.env.JWT_SECRET_CLIENTE},
-            {model: Preventista, type: 'Preventista', secret: process.env.JWT_SECRET_PREVENTISTA},
-            {model: Sucursal, type: 'Sucursal', secret: process.env.JWT_SECRET_SUCURSAL}
-        ]
-        let user, userType, secret_key, userModel = null
+        const {numero_celular , password} = req.body
+        const userTypes = [
+            { model: ClienteModel, type: 'CLIENTE', secret: process.env.JWT_SECRET_CLIENTE },
+            { model: PreventistaModel, type: 'PREVENTISTA', secret: process.env.JWT_SECRET_PREVENTISTA },
+            { model: SucursalModel, type: 'SUCURSAL', secret: process.env.JWT_SECRET_SUCURSAL }
+        ];
 
-        for(const {model, type, secret} of TP_user){
-            user = await model.findOne({correo: correoFL})
+        let user = null
+        let userType = null
+        let secret_key = null
+        let userModel = null
+        for(const {model, type, secret} of userTypes){
+            user = await model.findOne({ numero_celular: numero_celular });
             if(user){
-                userModel = model
-                secret_key = secret
                 userType = type
-                break
+                secret_key = secret
+                userModel = model
+                break;
             }
         }
         if(!user){
-            return res.status(404).json({msg: 'Usuario no encontrado'})
+            return res.status(404).json({ msg: 'Usuario no encontrado' });
         }
-        if(!await bcrypt.compare(passwordFL, user.password)){
-            return res.status(404).json({msg: 'Contraseña incorrecta'})
+        if(!await bcrypt.compare(password, user.password)){
+            return res.status(404).json({ msg: 'Contraseña incorrecta' });
         }
-        const token = jwt.sign(
-            {user_id: user._id, userType},
-            secret_key,
-            {expiresIn: process.env.JWT_EXPIRATION_TIME}
-        )
-
-        await userModel.findByIdAndUpdate(user._id, { token });
-        res.json({ 
-            msg: `Hola ${user.correo}, eres un usuario tipo ${userType}. Tu correo es ${user.correo}.`, 
-            token
-        });
         
-
     }catch(err){
         res.status(500).json({ msg: err.message });
     }
