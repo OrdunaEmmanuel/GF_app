84fad65ed53bc43a4875369454d56e14dda9cb7c
EliasibContre <cesareliasib018@gmail.com>
2025-04-11 02:00:56 -0600

    Update cors again

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 dotenv.config();
 
 app.use(cors({
-  origin: ['*'],
+  origin: '*',
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type, Authorization',
   credentials: true,
